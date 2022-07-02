69dbb44c4339673b68807e09e5bd2908ee3e87ac
Mikhael Lucas <developer.mikhael@gmail.com>
2022-07-02 10:17:18 -0300

    add setIsDrawerOpen when click on row player

--- src/components/drawer/player-results-list/row-player/index.js
+++ src/components/drawer/player-results-list/row-player/index.js
@@ -7,15 +7,10 @@ import './styles.css';
 import { PlayerContext } from '../../../player/context';
 
 const RowPlayer = ({currentPlayer}) => {
-    const { selectedPlayer, setSelectedPlayer, getCapitalizeName } = useContext(PlayerContext);
-
-    const handleOnClick = () => {
-        if (Object.keys(selectedPlayer) && selectedPlayer.ID === currentPlayer.ID) setSelectedPlayer({});
-        else setSelectedPlayer(currentPlayer);
-    }
+    const { selectedPlayer, setSelectedPlayer, getCapitalizeName, setIsDrawerOpen } = useContext(PlayerContext);
 
     return (
-        <div className='row-player' key={`player-result-${currentPlayer.ID}`} onClick={handleOnClick}>
+        <div className='row-player' key={`player-result-${currentPlayer.ID}`} onClick={() => { setIsDrawerOpen(false); setSelectedPlayer(currentPlayer) }}>
             <div style={{display: 'flex'}}>
                 <img src={currentPlayer.ImageURL} className='image-player'/>
                 <h6 className='name-player'>{getCapitalizeName(currentPlayer)}</h6>
